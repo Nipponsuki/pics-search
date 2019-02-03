32397ef2458110a76795c3c369d43a97826a1412
Alex <alex.alexeev23061991@gmail.com>
2019-02-03 05:07:58 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppBar from "material-ui/AppBar";
+
+const Navbar = props => {
+  return <AppBar title="PixaBayImageFinder" />;
+};
+
+export default Navbar;
