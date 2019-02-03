837d854a81d78adc94afb665529aa1f19bee0de2
Alex <alex.alexeev23061991@gmail.com>
2019-02-03 05:12:44 +0300

    Updates

---
+++ precache-manifest.d31c672142390426e831628207937e2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aef7e74f0b980793f3c7f247d103c955",
+    "url": "/Nipponsuki/pics-search.git/static/media/spinner.aef7e74f.gif"
+  },
+  {
+    "revision": "35b056766405415e369b",
+    "url": "/Nipponsuki/pics-search.git/static/js/runtime~main.35b05676.js"
+  },
+  {
+    "revision": "316a0c5fac4c826cbb75",
+    "url": "/Nipponsuki/pics-search.git/static/js/main.316a0c5f.chunk.js"
+  },
+  {
+    "revision": "5773aed251140a8a4864",
+    "url": "/Nipponsuki/pics-search.git/static/js/1.5773aed2.chunk.js"
+  },
+  {
+    "revision": "ce839e2752b27c6a0542a5833a811970",
+    "url": "/Nipponsuki/pics-search.git/index.html"
+  }
+];
